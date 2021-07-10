66161457b8fe42c98822eaf684608b4afaebcd87
Tiberiu <danielt.neagoe@gmail.com>
2021-07-10 02:02:21 -0400

    Added views.home

---
+++ vetoffice/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+pets = [
+   {"petname": "Fido", "animal_type": "dog"},
+   {"petname": "Clementine", "animal_type": "cat"},
+   {"petname": "Cleo", "animal_type": "cat"},
+   {"petname": "Oreo", "animal_type": "dog"},
+]
+
+
+def home(request):
+    context = {"name": "Djangoer", "pets": pets}
+    return render(request, "vetoffice/home.html", context)
